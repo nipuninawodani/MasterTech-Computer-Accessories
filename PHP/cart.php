bc643174c664e1aae599c42d48e653c7f084721e
Abdullah Ramees <73293868+AbdullahRamees@users.noreply.github.com>
2021-09-20 12:38:23 +0000

    create cart php (#8)
    all code included

---
+++ PHP/cart.php
@@ -0,0 +1,104 @@
+<?php
+	//geting connection for database
+	include 'Functions.php';
+	$link = dblink();
+	// If the user clicked the add to cart button on the product page we can check for the form data
+	if (isset($_POST['product_id'], $_POST['quantity'],$_POST['User_id']) && is_numeric($_POST['product_id']) && is_numeric($_POST['quantity']) && is_numeric($_POST['User_id'])) {
+		
+    // Set the post variables so we easily identify them, also make sure they are integer
+    	$product_id = (int)$_POST['product_id'];
+    	$quantity = (int)$_POST['quantity'];
+		$user_id= (int)$_POST['User_id'];
+		
+	//add cart product details to user cart items for 
+		$query=mysqli_query($link,"select max(id) as Iid from products_images");
+			$result=mysqli_fetch_array($query);
+			$ID=$result['Iid']+1;
+	   $add_to_cart_query="insert into cart_items(id,user_id,item_id,quantity_D) values ('$ID','$user_id','$item_id','$quantity')";
+		$add_to_cart_result=mysqli_query($link,$add_to_cart_query) or die(mysqli_error($link));
+		
+		
+		
+    // Prepare the SQL statement, we basically are checking if the product exists in our database
+    $stmt = $link->prepare('SELECT * FROM product WHERE  = ?');
+    $stmt->execute([$_POST['product_id']]);
+    // Fetch the product from the database and return the result as an Array
+    $product = $stmt->fetch(PDO::FETCH_ASSOC);
+    // Check if the product exists (array is not empty)
+    if ($product && $quantity > 0) {
+        // Product exists in database, now we can create/update the session variable for the cart
+        if (isset($_SESSION['cart']) && is_array($_SESSION['cart'])) {
+            if (array_key_exists($product_id, $_SESSION['cart'])) {
+                // Product exists in cart so just update the quanity
+                $_SESSION['cart'][$product_id] += $quantity;
+            } else {
+                // Product is not in cart so add it
+                $_SESSION['cart'][$product_id] = $quantity;
+            }
+        } else {
+            // There are no products in cart, this will add the first product to cart
+            $_SESSION['cart'] = array($product_id => $quantity);
+        }
+    }
+    // Prevent form resubmission...
+    exit;
+}
+
+	// Remove product from cart, check for the URL param "remove", this is the product id, make sure it's a number and check if it's in the cart
+	if (isset($_GET['remove']) && is_numeric($_GET['remove']) && isset($_SESSION['cart']) && isset($_SESSION['cart'][$_GET['remove']])) {
+    // Remove the product from the shopping cart
+    unset($_SESSION['cart'][$_GET['remove']]);
+}
+
+	// Update product quantities in cart if the user clicks the "Update" button on the shopping cart page
+	if (isset($_POST['update']) && isset($_SESSION['cart'])) {
+    // Loop through the post data so we can update the quantities for every product in cart
+    foreach ($_POST as $k => $v) {
+        if (strpos($k, 'quantity') !== false && is_numeric($v)) {
+            $id = str_replace('quantity-', '', $k);
+            $quantity = (int)$v;
+            // Always do checks and validation
+            if (is_numeric($id) && isset($_SESSION['cart'][$id]) && $quantity > 0) {
+                // Update new quantity
+                $_SESSION['cart'][$id] = $quantity;
+            }
+        }
+    }
+    // Prevent form resubmission...
+	//include cart page as header link  header('location: index.php?page=cart');
+    exit;
+}
+
+// Send the user to the place order page if they click the Place Order button, also the cart should not be empty
+if (isset($_POST['placeorder']) && isset($_SESSION['cart']) && !empty($_SESSION['cart'])) {
+	 //include placeorder page as header
+    exit;
+}
+
+// Send the user to the place order page if they click the Place Order button, also the cart should not be empty
+if (isset($_POST['placeorder']) && isset($_SESSION['cart']) && !empty($_SESSION['cart'])) {
+    header('Location: index.php?page=placeorder');
+    exit;
+}
+
+// Check the session variable for products in cart
+$products_in_cart = isset($_SESSION['cart']) ? $_SESSION['cart'] : array();
+$products = array();
+$subtotal = 0.00;
+// If there are products in cart
+if ($products_in_cart) {
+    // There are products in the cart so we need to select those products from the database
+    // Products in cart array to question mark string array, we need the SQL statement to include IN (?,?,?,...etc)
+    $array_to_question_marks = implode(',', array_fill(0, count($products_in_cart), '?'));
+    $stmt = $link->prepare('SELECT * FROM products WHERE id IN (' . $array_to_question_marks . ')');
+    // We only need the array keys, not the values, the keys are the id's of the products
+    $stmt->execute(array_keys($products_in_cart));
+    // Fetch the products from the database and return the result as an Array
+    $products = $stmt->fetchAll(PDO::FETCH_ASSOC);
+    // Calculate the subtotal
+    foreach ($products as $product) {
+        $subtotal += (float)$product['price'] * (int)$products_in_cart[$product['id']];
+    }
+}
+?>
+
